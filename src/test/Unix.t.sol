32c6db3048fb401b1734511712d1f3a52453f7c1
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2022-08-13 11:34:59 +0300

    Add unix lib

---
+++ src/test/Unix.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import {DSTest} from "ds-test/test.sol";
+import {Unix} from "unix/Unix.sol";
+
+contract UnixTest is DSTest {
+    // using Unix for *;
+
+    function setUp() public {}
+
+    function testUnix1() public {}
+}
